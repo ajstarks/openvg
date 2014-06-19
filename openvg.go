70057c79504579a4ce59e4c5bf485fcfd42eb59a
Anthony Starks <ajstarks@gmail.com>
2014-06-19 02:14:07 -0400

    update Go build to include -lbcm_host and -lEGL

--- openvg.go
+++ openvg.go
@@ -3,7 +3,7 @@ package openvg
 
 /*
 #cgo CFLAGS:   -I/opt/vc/include -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads 
-#cgo LDFLAGS:  -L/opt/vc/lib -lGLESv2 -ljpeg
+#cgo LDFLAGS:  -L/opt/vc/lib -lGLESv2 -lEGL -lbcm_host -ljpeg
 #include "VG/openvg.h"
 #include "VG/vgu.h"
 #include "EGL/egl.h"
