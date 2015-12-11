36c30752618d48288b093609564986ee23b991e2
Anthony Starks <ajstarks@gmail.com>
2015-12-10 20:27:34 -0500

    splash: add positioning flags

--- go-client/splash/splash.go
+++ go-client/splash/splash.go
@@ -36,11 +36,13 @@ func getimage(image_path string, w, h int, resized bool) (image.Image, error) {
 }
 
 func main() {
-	var resize = flag.Bool("resize", false, `Resize image to fit the screen.`)
-	var bgcolor = flag.String("bg", "black", `Background color (named color or rgb(r,g,b)).`)
-	var fgcolor = flag.String("fg", "white", `text color (named color or rgb(r,g,b)).`)
+	var resize = flag.Bool("resize", false, "Resize image to fit the screen.")
+	var bgcolor = flag.String("bg", "black", "Background color (named color or rgb(r,g,b)).")
+	var fgcolor = flag.String("fg", "white", "text color (named color or rgb(r,g,b)).")
 	var title = flag.String("t", "", "text annotation")
 	var fontsize = flag.Float64("fp", 2.0, "fontsize %")
+	var px = flag.Float64("px", 50, "x position %")
+	var py = flag.Float64("py", 50, "y position %")
 	var texty = flag.Float64("ty", 0, "text y %")
 	var exit_code int
 	defer func() {
@@ -84,18 +86,29 @@ Default is to put this image to the center.
 
 	openvg.Start(w, h)
 	openvg.BackgroundColor(*bgcolor)
-	x, y := openvg.VGfloat(w)/2-openvg.VGfloat(imw)/2, openvg.VGfloat(h)/2-openvg.VGfloat(imh)/2
+	var x, y openvg.VGfloat
+	if *px < 0 || *px > 100 {
+		x = openvg.VGfloat(w)/2 - openvg.VGfloat(imw)/2
+	} else {
+		x = openvg.VGfloat(w)*openvg.VGfloat(*px/100) - openvg.VGfloat(imw)/2
+	}
+
+	if *py < 0 || *py > 100 {
+		y = openvg.VGfloat(h)/2 - openvg.VGfloat(imh)/2
+	} else {
+		y = openvg.VGfloat(h)*openvg.VGfloat(*py/100) - openvg.VGfloat(imh)/2
+	}
 	openvg.Img(x, y, img)
 	if len(*title) > 0 {
-		var yp openvg.VGfloat
+		var typ openvg.VGfloat
 		fs := openvg.VGfloat(*fontsize/100.0) * openvg.VGfloat(w)
 		if *texty == 0 {
-			yp = y - fs*1.5
+			typ = y - fs*1.5
 		} else {
-			yp = openvg.VGfloat(h) * openvg.VGfloat(*texty/100)
+			typ = openvg.VGfloat(h) * openvg.VGfloat(*texty/100)
 		}
 		openvg.FillColor(*fgcolor)
-		openvg.TextMid(x+openvg.VGfloat(imw)/2, yp, *title, "sans", int(fs))
+		openvg.TextMid(x+openvg.VGfloat(imw)/2, typ, *title, "sans", int(fs))
 	}
 	openvg.End()
 
