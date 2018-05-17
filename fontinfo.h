91430dbd5babf6188f717c170daa24056b3f3bcc
gthomas <gthomas@geologging.com>
2018-05-17 15:11:42 +0100

    formatting

--- fontinfo.h
+++ fontinfo.h
@@ -4,18 +4,17 @@
 #if defined(__cplusplus)
 extern "C" {
 #endif
-	typedef struct {
-		const short *CharacterMap;
-		const int *GlyphAdvances;
-		int Count;
-		int descender_height;
-		int font_height;
-		VGPath Glyphs[500];
-	} Fontinfo;
+    typedef struct {
+        const short *CharacterMap;
+        const int *GlyphAdvances;
+        int Count;
+        int descender_height;
+        int font_height;
+        VGPath Glyphs[500];
+    } Fontinfo;
 
-	extern Fontinfo SansTypeface, SerifTypeface, MonoTypeface;
-
+    extern Fontinfo SansTypeface, SerifTypeface, MonoTypeface;
 #if defined(__cplusplus)
 }
-#endif				// OPENVG_FONTINFO_H
+#endif
 #endif
