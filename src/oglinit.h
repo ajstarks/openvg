2b966bf767a0427f16f05a36568ac9b993d33271
gthomas <gthomas@geologging.com>
2018-06-07 09:34:52 +0100

    doc

--- src/oglinit.h
+++ src/oglinit.h
@@ -1,19 +1,31 @@
 
+/**
+ *
+ */
 typedef struct {
-    // Screen dimentions
+
+    /** Screen width, in pixels. */
     uint32_t screen_width;
+
+    /** Screen height, in pixels. */
     uint32_t screen_height;
-    // Window dimentions
+
+    /** Window `x` coordinate. */
     int32_t window_x;
+
+    /** Window `y` coordinate. */
     int32_t window_y;
+
+    /** Window width, in pixels. */
     uint32_t window_width;
+
+    /** Window height, in pixels. */
     uint32_t window_height;
-    // dispman window
+
     DISPMANX_ELEMENT_HANDLE_T element;
 
     // EGL data
     EGLDisplay display;
-
     EGLSurface surface;
     EGLContext context;
 } STATE_T;
